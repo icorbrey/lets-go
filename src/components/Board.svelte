b8ee2072db81b7e6a936585077b2240a54db2a8b
Isaac <icorbrey@gmail.com>
2023-03-07 13:32:55 -0500

    feat: Create basic board components

---
+++ src/components/Board.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import Tile from './Tile.svelte';
+
+	export let size = 19;
+</script>
+
+<div class="board">
+	{#each { length: size } as _, y}
+		<div class="row">
+			{#each { length: size } as _, x}
+				<Tile {x} {y} {size} />
+			{/each}
+		</div>
+	{/each}
+</div>
+
+<style lang="scss">
+	.board {
+		display: flex;
+		flex-flow: column nowrap;
+
+		.row {
+			display: flex;
+			flex-flow: row nowrap;
+		}
+	}
+</style>
---
+++ src/components/Tile.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	export let x: number;
+	export let y: number;
+	export let size: number;
+</script>
+
+<div class="tile">
+	{#if y !== 0}
+		<div class="north" />
+	{/if}
+	{#if x < size - 1}
+		<div class="east" />
+	{/if}
+	<div class="center" />
+	{#if y < size - 1}
+		<div class="south" />
+	{/if}
+	{#if x !== 0}
+		<div class="west" />
+	{/if}
+</div>
+
+<style lang="scss">
+	.tile {
+		display: grid;
+		grid-template-rows: 20px 5px 20px;
+		grid-template-columns: 20px 5px 20px;
+		grid-template-areas:
+			'.    north .    '
+			'west center east'
+			'.    south .    ';
+
+		> div {
+			background-color: black;
+		}
+
+		.north {
+			grid-area: north;
+		}
+
+		.east {
+			grid-area: east;
+		}
+
+		.center {
+			grid-area: center;
+		}
+
+		.south {
+			grid-area: south;
+		}
+
+		.west {
+			grid-area: west;
+		}
+	}
+</style>
